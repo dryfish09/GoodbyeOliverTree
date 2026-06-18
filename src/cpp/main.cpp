fa73430c0d568fbb5d5b2c7d2045112707bfd0d8
Dry Fish <realmg51@gmail.com>
2026-06-18 18:24:31 +0700

    C++

---
+++ src/cpp/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main() {
+  bool OliverTreeIsDie = true;
+  if (OliverTreeIsDie) {
+       std::cout<<"Goodbye Oliver, I'll never see you again"<<std::endl;
+  } else {
+       std::cout<<"That's magic! You are alive!"<<std::endl;
+  }
+  return 0;
+}
