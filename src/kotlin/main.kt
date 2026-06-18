3d37567f779f3fc0600117d23d1d3e1507699161
Dry Fish <realmg51@gmail.com>
2026-06-18 18:37:20 +0700

    Kotlin

---
+++ src/kotlin/main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val OliverTreeIsDie = true
+    if (OliverTreeIsDie) {
+        println("Goodbye Oliver Tree, my idol. I know you never comeback because it's real")
+    } else {
+        println("It's never happen,but I hope one day...")
+    }
+}
